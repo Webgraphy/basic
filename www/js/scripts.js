98504ca18ced61718a1f99f675074e572498fd74
Twist <a.dadaev@bytexgames.ru>
2017-10-26 16:53:43 +0300

    Good

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -1,4 +1,43 @@
 $(function () {
+    $(document).on('submit', '.form', function (e) {
+        e.preventDefault();
+        var form = this;
+
+        var errors = 0;
+
+        $(this).find('input[type="text"]:visible, textarea.required, input[type="checkbox"], select').each(function(data) {
+            if ($(this).val() == '') {
+                $(this).parent().addClass('has-error has-feedback');
+
+                errors++;
+            } else {
+                $(this).parent().removeClass('has-error has-feedback');
+            }
+
+            if ($(this).attr('type') == 'checkbox') {
+                if ($(this).is(':checked')) {
+                    $(this).parents('.checkbox').removeClass('has-error has-feedback');
+                } else {
+                    $(this).parents('.checkbox').addClass('has-error has-feedback');
+
+                    errors++;
+                }
+            }
+        });
+
+        if (errors == 0) {
+            $.ajax({
+                type: $(this).attr('method'),
+                url: $(this).attr('action'),
+                data: $(this).serialize(),
+                resetForm: true,
+                success: function (result) {
+                    $(form).html('<h3>РЎРїР°СЃРёР±Рѕ!</h3><p>РРЅС„РѕСЂРјР°С†РёСЏ СѓСЃРїРµС€РЅРѕ РѕС‚РїСЂР°РІР»РµРЅР°.');
+                }
+            });
+        }
+    });
+
     $('.owl-carousel').owlCarousel({
         loop: true,
         margin: 0,
@@ -13,8 +52,11 @@ $(function () {
                 items: 4
             },
             1000:{
-                items: 7
+                items: 4
             }
         }
-    })
+    });
+
+    new WOW().init();
+    $(':input').inputmask();
 });
