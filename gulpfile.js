a5b60554bc18270cd44e891b974b37f1a3573052
Twist <a.dadaev@bytexgames.ru>
2021-03-09 13:00:40 +0300

    NEW

--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,37 @@
 'use strict';
 
 var gulp = require('gulp');
-var jade = require('gulp-jade');
+var fileinclude = require('gulp-file-include');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync');
-var image = require('gulp-image');
 
-gulp.task('sass', function () {
+
+function css() {
     return gulp.src('./src/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
-        }))
         .pipe(gulp.dest('./www/css'))
         .pipe(browserSync.reload({stream: true}));
-});
+}
 
-gulp.task('templates', function () {
-    gulp.src('./src/templates/*.jade')
-        .pipe(jade({
-            pretty: true
+function templates () {
+    return gulp.src(['./src/templates/index.html'])
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
         }))
         .pipe(gulp.dest('./www/'))
         .pipe(browserSync.reload({stream: true}));
-});
+}
 
-gulp.task('images', function () {
-    gulp.src('./images/**/*')
-        .pipe(image({
-            pngquant: true,
-            optipng: false,
-            zopflipng: true,
-            jpegRecompress: false,
-            mozjpeg: true,
-            guetzli: false,
-            gifsicle: true,
-            svgo: true,
-            concurrent: 10,
-            quiet: true // defaults to false
-        }))
-        .pipe(gulp.dest('./newimages'))
-});
-
-gulp.task('browser-sync', function () {
-    browserSync({
+function browser() {
+    browserSync.init({
         server: {
             baseDir: "./www"
         }
     });
-});
 
-gulp.task('default', gulp.series('browser-sync', 'sass', 'templates'), function () {
-    gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
-    gulp.watch('./src/templates/**/*.jade', gulp.series('templates'));
-    gulp.watch('./src/*.html').on('change', () => {
-        browserSync.reload();
-        done();
-    });
-});
+    gulp.watch('./src/sass/**/*.scss', { usePolling: true }, css);
+    gulp.watch('./src/templates/**/*.html', { usePolling: true }, templates);
+}
+
+exports.default = gulp.series(css, templates, browser);
